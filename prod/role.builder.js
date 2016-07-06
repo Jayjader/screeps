74dc9c24ba672407d80fec73d2e10124a67044a6
Noah Jay <jayn@bde.enseeiht.fr>
2016-07-06 18:23:24 +0200

    testing creep say command

--- prod/role.builder.js
+++ prod/role.builder.js
@@ -9,12 +9,12 @@ var roleBuilder = {
         // stop building when out of energy
         if (creep.memory.building && creep.carry.energy == 0) {
             creep.memory.building = false;
-            creep.say('building!');
+            creep.say('refilling!');
         }
         // start building when refilled completely
         if (!creep.memory.building && creep.carry.energy == creep.carryCapacity) {
             creep.memory.building = true;
-            creep.say('refilling!');
+            creep.say('building!');
         }
 
         // When building: find something to build
