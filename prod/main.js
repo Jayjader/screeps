65afb8bb8b75c958e943a5374ffc353486cd874a
Noah Jay <jayn@bde.enseeiht.fr>
2016-07-06 20:21:32 +0200

    fixing display function

--- prod/main.js
+++ prod/main.js
@@ -2,7 +2,7 @@ var roleHarvester = require('role.harvester');
 var roleBuilder = require('role.builder');
 var roleUpgrader = require('role.upgrader');
 
-function  creepNumbers(harvesters, upgraders, builders, repairers) {
+function  creepNumbers(harvesters, builders, upgraders, repairers) {
     console.log( + builders.length + ' builders; ' +
                 + harvesters.length + ' harvesters; ' +
                 + upgraders.length + ' upgraders; ' +
