1fbfb751109e3a3eff4b4c16ea4e824bfe3db58a
Noah Jay <jayn@bde.enseeiht.fr>
2016-07-06 16:18:50 +0200

    Added Memory cleanup and auto-spawning of harvesters

--- prod/main.js
+++ prod/main.js
@@ -1,11 +1,27 @@
-var roleHarvester = require('role.harvester.js');
-var roleBuilder = require('role.builder.js');
-var roleUpgrader = require('role.upgrader.js');
+var roleHarvester = require('role.harvester');
+var roleBuilder = require('role.builder');
+var roleUpgrader = require('role.upgrader');
 
-/* Main loop */
+// Main loop
 module.exports.loop = function () {
 
-    /* Order creeps */
+    // Memory clean-up
+    for (var name in Memory.creeps) {
+        if (!Game.creeps[name]) {
+            console.log('Clearing non-existing creep memory: ', name);
+            delete Memory.creeps[name];
+        }
+    }
+
+    // Auto-spawning
+    var harvesters = _.filter(Game.creeps, (creep) => creep.memory.role == 'harvester');
+    if (harvesters.length < 3) {
+        var newName = game.spawns.Spawn1.createCreep([WORK, CARRY, MOVE],
+                undefined, {role : 'harvester'});
+        console.log('Spawned new harvester: ' + newName);
+    }
+
+    // Order creeps
     for (creep in Game.creeps) {
         switch (creep.memory.role) {
             case 'harvester':
