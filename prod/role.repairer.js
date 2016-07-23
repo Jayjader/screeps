49bc9e23e77853dd8b3ce1d36c475c86323da617
Jayjader <nijagameexpert@gmail.com>
2016-07-23 21:18:22 +0200

    fixed repairers not moving to energy sources

--- prod/role.repairer.js
+++ prod/role.repairer.js
@@ -57,7 +57,7 @@ var roleRepairer = {
         }
         // When not repairing: find somewhere to harvest
         else {
-            var sources = creep.pos.findClosestByPath(FIND_SOURCES);
+            var sources = creep.room.find(FIND_SOURCES);
 
             // Try to harvest. If out of range then move closer.
             if (creep.harvest(sources[0]) == ERR_NOT_IN_RANGE) {
